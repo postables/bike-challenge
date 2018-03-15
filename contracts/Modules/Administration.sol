d36f1eb5203496be6ebc018d5b6e1751a8e4ef4c
postables <postables@none.com>
2018-03-15 13:08:27 -0700

    Initial commit, added usual base contracts (safe math, admin, erc20 interface)

---
+++ contracts/Modules/Administration.sol
@@ -0,0 +1,47 @@
+pragma solidity 0.4.21;
+
+contract Administration {
+
+    address public owner;
+    address public admin;
+
+    event AdminSet(address _admin);
+    event NewOwner(address _owner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier permissioned() {
+        require(msg.sender == owner || msg.sender == admin);
+        _;
+    }
+
+    function Administration() {
+        owner = msg.sender;
+        admin = msg.sender;
+    }
+
+    function setAdmin(
+        address _admin)
+        public
+        onlyOwner
+        returns (bool)
+    {
+        admin = _admin;
+        emit AdminSet(_admin);
+        return true;
+    }
+
+    function changeOwner(
+        address _owner)
+        public
+        onlyOwner
+        returns (bool)
+    {
+        owner = _owner;
+        emit NewOwner(_owner);
+        return true;
+    }
+}
